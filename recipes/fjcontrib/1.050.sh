41639368210591f10fe5d74fbb088dc1dcaf1b0f
matplo <ploskon@gmail.com>
2023-01-21 10:55:13 -0800

    compile fjcontrib using  or get it from yasp if unset

--- recipes/fjcontrib/1.050.sh
+++ recipes/fjcontrib/1.050.sh
@@ -8,26 +8,29 @@ local_file={{workdir}}/fjcontrib-{{version}}.tar.gz
 tar zxvf {{local_file}}
 srcdir={{workdir}}/fjcontrib-{{version}}
 cd {{srcdir}}
+if [ -z "${fastjet_prefix}" ]; then
+	   fastjet_prefix=$({{yasp}} -q feature prefix -i fastjet)
+fi
 # this produces only static libs
-./configure --fastjet-config={{prefix}}/bin/fastjet-config --prefix={{prefix}} && make -j {{n_cores}} all && make check && make install
+./configure --fastjet-config=${fastjet_prefix}/bin/fastjet-config --prefix=${fastjet_prefix} && make -j {{n_cores}} all && make check && make install
 # add a cmake for dynamic libs!
-if [ $? -eq 0 ] 
+if [ $? -eq 0 ]
 then
-	make clean 
-	./configure --fastjet-config={{prefix}}/bin/fastjet-config --prefix={{prefix}} CXXFLAGS=-fPIC && make -j {{n_cores}} all && make check && make install
+	make clean
+	./configure --fastjet-config=${fastjet_prefix}/bin/fastjet-config --prefix=${fastjet_prefix} CXXFLAGS=-fPIC && make -j {{n_cores}} all && make check && make install
 	contribs=$(./configure --list)
 	for c in ${contribs}
 	do
 		cd ${c}
 		rm *example*.o
-		shlib={{prefix}}/lib/lib${c}.so
-		{{CXX}} -fPIC -shared -o ${shlib} *.o -Wl,-rpath,{{prefix}}/lib -L{{prefix}}/lib -lfastjettools -lfastjet
+		shlib=${fastjet_prefix}/lib/lib${c}.so
+		{{CXX}} -fPIC -shared -o ${shlib} *.o -Wl,-rpath,${fastjet_prefix}/lib -L${fastjet_prefix}/lib -lfastjettools -lfastjet
 		if [ -f ${shlib} ]; then
 			echo "[i] shared lib created ${shlib}"
 		else
 			echo "[i] shared lib NOT created ${shlib}"
 		fi
-		cd {{srcdir}}	
+		cd {{srcdir}}
 	done
 fi
 exit $?
