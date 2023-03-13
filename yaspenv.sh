1b5718d8c60ccc64b24f4ac99025f91660379fb4
matplo <ploskon@gmail.com>
2023-03-13 15:16:47 -0700

    ditch pipenv for venv

--- yaspenv.sh
+++ yaspenv.sh
@@ -17,23 +17,47 @@ THISD=$(thisdir)
 
 cd ${THISD}
 
+
 # echo "[current dir:] ${THISD}"
 
-_venv=$(pipenv --venv)
-echo ${_venv}
-if [ -d "${_venv}" ]; then
-	if [ -z "$@" ]; then
-		pipenv shell
-	else
-		pipenv run $@
-	fi
-else
-	current_python_version=$(python3 -c "import sys; print('.'.join([str(s) for s in sys.version_info[:3]]));")
-	pipenv --python ${current_python_version}
-	pipenv install pyyaml 
-	pipenv run ./yasp.py --configure $@
-	pipenv run ./yasp.py --install yasp -m
-	pipenv shell
+venvdir=${THISD}/venvyasp
+cmnd=$@
+
+if [ ! -d ${venvdir} ]; then
+	echo "[i] creating venv"
+	python3 -m venv ${venvdir}
 fi
+
+tmpfile=$(mktemp)	
+if [ -d ${venvdir} ]; then
+	echo "export PS1=\"\e[32;1m[\u\e[31;1m@\h\e[32;1m]\e[34;1m\w\e[0m\n> \"" > ${tmpfile}
+	echo "source ${venvdir}/bin/activate" >> ${tmpfile}
+	if [ -z "${cmnd}" ]; then
+		/bin/bash --init-file ${tmpfile} -i
+	else
+		echo "[i] exec ${tmpfile}"
+		echo "${cmnd}" >> ${tmpfile}
+		chmod +x ${tmpfile}
+		${tmpfile}
+	fi
+	#-s < .activate
+fi
+
+#_venv=$(pipenv --venv)
+#echo ${_venv}
+#if [ -d "${_venv}" ]; then
+#	if [ -z "$@" ]; then
+#		pipenv shell
+#	else
+#		pipenv run $@
+#	fi
+#else
+#	current_python_version=$(python3 -c "import sys; print('.'.join([str(s) for s in sys.version_info[:3]]));")
+#	pipenv --python ${current_python_version}
+#	pipenv install pyyaml 
+#	pipenv run ./yasp.py --configure $@
+#	pipenv run ./yasp.py --install yasp -m
+#	pipenv shell
+#fi
 
 cd ${savedir}
