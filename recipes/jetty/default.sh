fd8276a808b0ba9bebf92fb0bf6ef0cf87185714
matplo <ploskon@gmail.com>
2023-03-20 15:25:10 -0700

    take the latest; root up

--- recipes/jetty/default.sh
+++ recipes/jetty/default.sh
@@ -7,7 +7,8 @@ softprefix=$({{yasp}} -q feature prefix)
 module use ${softprefix}/modules
 module avail
 
-for pack in fastjet/3.4.0 fjcontrib/1.051 HepMC3/3.2.5 LHAPDF6/6.5.3 root/6.26.10 pythia8/8308
+# for pack in fastjet/3.4.0 fjcontrib/1.051 HepMC3/3.2.5 LHAPDF6/6.5.3 root/6.26.10 pythia8/8308
+for pack in fastjet fjcontrib HepMC3 LHAPDF6 root pythia8
 do
 	module list
 	{{yasp}} -i ${pack} -m
