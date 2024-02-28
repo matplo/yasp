e1389c68b8fd4c86a619825f68aca826d6221136
Ezra Lesser <ezralesser@gmail.com>
2024-02-28 14:41:36 +0000

    Software updates

---
+++ recipes/fastjet/3.4.2-threadsafety.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd {{workdir}}
+version=3.4.2
+url=http://fastjet.fr/repo/fastjet-{{version}}.tar.gz
+local_file={{workdir}}/fastjet-{{version}}.tar.gz
+{{yasp}} --download {{url}} --output {{local_file}}
+tar zxvf {{local_file}}
+srcdir={{workdir}}/fastjet-{{version}}
+
+cd {{builddir}}
+if [ -d "${CGAL_DIR}" ]; then
+	# cgal_opt="--with-cgaldir=${CGAL_DIR} --enable-cgal-header-only"
+	# echo "$CGAL_DIR"
+	cgal_opt="--enable-cgal-header-only --with-cgaldir=${CGAL_DIR}"
+else
+	cgal_opt="--disable-cgal"
+fi
+other_opts="--enable-allcxxplugins --enable-thread-safety --enable-allplugins"
+# not enabling the swig interface --enable-pyext
+system=$(gcc -dumpmachine)
+echo "{{srcdir}}/configure --prefix={{prefix}} --build=${system} --host=${system} ${cgal_opt} ${other_opts} "
+{{srcdir}}/configure --prefix={{prefix}} --build=${system} --host=${system} ${cgal_opt} ${other_opts} && make -j {{n_cores}} && make install
+exit $?
