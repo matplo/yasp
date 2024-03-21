46b31d06d3fb20d2a0af6ccedd61a5ef8b206990
matplo <ploskon@gmail.com>
2024-03-21 14:52:07 -0700

    pythia 8245

---
+++ recipes/pythia8/8245.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd {{workdir}}
+version=8245
+url=https://pythia.org/download/pythia82/pythia{{version}}.tgz
+local_file={{workdir}}/pythia{{version}}.tar.gz
+{{yasp}} --download {{url}} --output {{local_file}}
+tar zxvf {{local_file}}
+srcdir={{workdir}}/pythia{{version}}
+
+cd {{srcdir}}
+# obsolete --enable-shared
+if [ -d "${LHAPDF6_DIR}" ]; then
+	LHAPDF6OPT=--with-lhapdf6=${LHAPDF6_DIR}
+else
+	LHAPDF6OPT=""
+fi
+
+if [ -d "${FASTJET_DIR}" ]; then
+	FASTJET3OPT=--with-fastjet3=${FASTJET_DIR}
+else
+	FASTJET3OPT=""
+fi
+
+if [ -d "${ROOTSYS}" ]; then
+	ROOTOPT=--with-root=${ROOTSYS}
+else
+	ROOTOPT=""
+fi
+
+{{srcdir}}/configure --prefix={{prefix}} ${LHAPDF6OPT} ${ROOTOPT} ${FASTJET3OPT} && make -j {{n_cores}} && make install
+# --with-python-include="$(python -c "from sysconfig import get_paths; info = get_paths(); print(info['include'])")"
+exit $?
