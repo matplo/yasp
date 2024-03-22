144f7ff954f286497fc0f0d03affd5f628235e67
matplo <ploskon@gmail.com>
2024-03-21 19:40:11 -0700

    up root version

--- recipes/root/default.sh
+++ recipes/root/default.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd {{workdir}}
-version=6.28.12
-#yasp --set version=6.26.10
+# version=6.28.12
+version=6.30.04
 url=https://root.cern/download/root_v{{version}}.source.tar.gz
 local_file={{workdir}}/root_v{{version}}.tar.gz
 {{yasp}} --download {{url}} --output {{local_file}}
