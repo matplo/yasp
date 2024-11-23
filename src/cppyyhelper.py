f34d1f5d8aec9c8da0e6b0ae51bfe6fedec0af13
matplo <ploskon@gmail.com>
2024-11-22 23:47:50 -0800

    running in jupyter; saul tested

--- src/cppyyhelper.py
+++ src/cppyyhelper.py
@@ -2,16 +2,59 @@ import yasp
 import sys
 import os
 import cppyy 
+import subprocess
 
-    
+def is_running_in_jupyter():
+    try:
+        from IPython import get_ipython
+        _get_ipy = get_ipython()
+        if _get_ipy is None:
+          return False
+        if 'IPKernelApp' in get_ipython().config:
+            return True
+    except ImportError:
+        pass
+    return False
+
+def add_to_ld_library_path(path):
+		if 'LD_LIBRARY_PATH' in os.environ:
+				if path not in os.environ['LD_LIBRARY_PATH'].split(':'):
+					os.environ['LD_LIBRARY_PATH'] = path + ':' + os.environ['LD_LIBRARY_PATH']
+		else:
+				os.environ['LD_LIBRARY_PATH'] = path
+
+        
 class YaspCppyyHelper(yasp.GenericObject):
 	_instance = None
 	def __new__(cls):
 		if cls._instance is None:
 			print('[i] Creating YaspCppyyHelper singleton.', file=sys.stderr)
 			cls._instance = super(YaspCppyyHelper, cls).__new__(cls)
+			cls._instance.jupyter = is_running_in_jupyter()
+			print('[w] Running in Jupyter:', cls._instance.jupyter, file=sys.stderr)
 		return cls._instance
 
+
+	def reload_yasp_cppyy_paths(self):
+		print('[i] Reloading YaspCppyyHelper paths', file=sys.stderr)
+		_data = yasp.YaspSingletonData()
+		if self.paths_include is None:
+			self.paths_include = []
+		for _path in _data.cppyy_paths:
+			cppyy.add_include_path(_path)
+		gSys = cppyy.gbl.gSystem
+		for _path in sys.path:
+			if os.path.isdir(_path):
+				add_to_ld_library_path(_path)
+				cppyy.add_library_path(_path)
+				if gSys is not None:
+					gSys.AddDynamicPath(_path)
+    # make sure python lib dir is in the path
+		_python_lib_dir = yasp.get_python_lib_dir()
+		cppyy.add_library_path(_python_lib_dir)
+		if gSys is not None:
+			gSys.AddDynamicPath(_python_lib_dir)
+  
 	def cppyy_add_paths(self, *packages):
 		if self.paths_include is None:
 			self.paths_include = []
@@ -36,7 +79,58 @@ class YaspCppyyHelper(yasp.GenericObject):
 					cppyy.add_library_path(_lib64_path)
 					self.paths_lib.append(_lib64_path)
 
+	def find_lib_candidate_in_sys_path(self, libname):
+		_lname = os.path.basename(libname)
+		extensions = ['.so', '.dll', '.dylib', '.sl', '.dl', '.a']
+		paths_to_search = sys.path
+		for path in paths_to_search:
+				for ext in extensions:
+						candidate = os.path.join(path, _lname + ext)
+						if os.path.isfile(candidate):
+								return candidate
+		_lname = 'lib' + os.path.basename(libname)
+		for path in paths_to_search:
+				for ext in extensions:
+						candidate = os.path.join(path, _lname + ext)
+						if os.path.isfile(candidate):
+								return candidate
+		return None
+
+	def show_linked_libraries(self, lib_path):
+			try:
+					result = subprocess.run(['ldd', lib_path], capture_output=True, text=True, check=True)
+					print(result.stdout)
+			except subprocess.CalledProcessError as e:
+					print(f"Error running ldd on {lib_path}: {e}", file=sys.stderr)
+
+	def load_dependencies(self, slib_path):
+			print('[i] Loading dependencies for:', slib_path)
+			try:
+					if os.path.isfile(slib_path) is False:
+						lib_path = os.path.basename(slib_path)
+						lib_path = self.find_lib_candidate_in_sys_path(lib_path)
+						if lib_path is None:
+								print(f"[w] ::load dependencies: Could not find library {slib_path}", file=sys.stderr)
+								return
+					else:
+						lib_path = slib_path
+					result = subprocess.run(['ldd', lib_path], capture_output=True, text=True, check=True)
+					for line in result.stdout.splitlines():
+							parts = line.split()
+							if len(parts) >= 3 and parts[1] == '=>':
+									dep_path = parts[2]
+									if 'not' in  parts[2] and 'found' in parts[3]:
+										dep_path = parts[0].strip()
+									try:
+											cppyy.load_library(dep_path)
+											print(f"[i] Loaded dependency: {dep_path}")
+									except Exception as e:
+											print(f"[i] Failed to load dependency: {dep_path}, error: {e}", file=sys.stderr)
+			except subprocess.CalledProcessError as e:
+					print(f"Error running ldd on {lib_path}: {e}", file=sys.stderr)
+            
 	def load(self, packages=[], libs=[], headers=[]):
+		self.reload_yasp_cppyy_paths()
 		if self.loaded_packages is None:
 			self.loaded_packages = []
 		if self.loaded_libs is None:
@@ -50,8 +144,26 @@ class YaspCppyyHelper(yasp.GenericObject):
 			cppyy.include(fn)			
 		for p in libs:
 			if p not in self.loaded_libs:
-				cppyy.load_library(p)
-				self.loaded_libs.append(p)
+				loaded = False
+				try:
+					cppyy.load_library(p)
+					self.loaded_libs.append(p)
+					loaded = True
+				except Exception as e:
+					print('[e] Failed to load library:', p, e, file=sys.stderr)
+				if loaded is False:
+					# try to find the library in the system path
+					_candidate = self.find_lib_candidate_in_sys_path(p)
+					if _candidate is not None:
+						print('[i] Trying to load library:', _candidate, 'instead of', p, file=sys.stderr)
+						if self.jupyter:
+							self.load_dependencies(_candidate)
+						#_load_ret = cppyy.gbl.gSystem.Load(_candidate)
+						# print('[i] Load return:', _load_ret, file=sys.stderr)
+						cppyy.load_library(_candidate)
+						self.loaded_libs.append(p)
+					else:
+						print('[e] No candidate found for:', p, file=sys.stderr)
 
 	def get(self, symbol = '', verbose=False):
 		try:
