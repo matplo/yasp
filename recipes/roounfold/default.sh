afe60833e6e454f8dab61929c12fb2b254b31ab6
matplo <ploskon@gmail.com>
2025-02-04 14:33:31 -0800

    up root version; make sure roounfold tagged version

--- recipes/roounfold/default.sh
+++ recipes/roounfold/default.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 export RUGITREPO=https://gitlab.cern.ch/RooUnfold/RooUnfold.git
-roounfold_version=master
+version=master
 
 #ezrasru=$(get_opt "ezra" $@)
 #if [ ! -z ${ezrasru} ]; then
 #    export RUGITREPO=https://gitlab.cern.ch/elesser/RooUnfold.git
-#    roounfold_version=master
+#    version=master
 #fi
 
-echo "[i] Using RooUnfold at ${RUGITREPO} ${roounfold_version}"
-echo "    version=${roounfold_version}"
+echo "[i] Using RooUnfold at ${RUGITREPO} {{version}}"
+echo "    version={{version}}"
 
 cd {{workdir}} && rm -rf RooUnfold
 git clone ${RUGITREPO}
 srcdir={{workdir}}/RooUnfold
 build_dir={{workdir}}/build
-if [ ! {{roounfold_version}} == master ]; then
-    git checkout {{roounfold_version}}
+if [ "{{version}}" != "master" ]; then
+    cd ${srcdir}
+    git checkout {{version}}
 fi
 cd {{build_dir}}
 cmake {{srcdir}} -DCMAKE_INSTALL_PREFIX={{prefix}} -DCMAKE_BUILD_TYPE=Release
