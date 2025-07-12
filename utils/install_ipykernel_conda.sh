a7478ac6720935dd1f7d8dcf9a1abafc6867426b
matplo <ploskon@gmail.com>
2025-07-11 22:53:41 -0700

    dir based name

--- utils/install_ipykernel_conda.sh
+++ utils/install_ipykernel_conda.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-conda install -y conda-forge::ipykernel
+kernel_name=$(echo $CONDA_DEFAULT_ENV | sed 's/\//-/g' | sed 's/ //g' | sed 's/--/_/g' | sed 's/-/_/g' | sed 's/^_//g' | sed 's/_$//g')
+kernel_display_name=${kernel_name}
 
-python -m ipykernel install --user --name conda_env_yasp_python_3.11 --display-name conda_env_yasp_python_3.11 --env LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:\${LD_LIBRARY_PATH} --env PYTHONPATH ${PYTHONPATH}:\${PYTHONPATH} --env PATH ${PATH}:\${PATH} --env CONDA_PREFIX ${CONDA_PREFIX}:\${CONDA_PREFIX} --env CONDA_DEFAULT_ENV ${CONDA_DEFAULT_ENV}:\${CONDA_DEFAULT_ENV} --env YASP_VENV_SH ${YASP_VENV_SH} --env YASP_VENV_TYPE ${YASP_VENV_TYPE}
+echo "Installing IPython kernel for conda environment: ${kernel_name}"
+
+conda install -y conda-forge::ipykernel
+if [ $? -ne 0 ]; then
+		echo "Failed to install ipykernel"
+		exit 1
+fi
+
+python -m ipykernel install --user --name ${kernel_name} --display-name "${kernel_display_name}" --env LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:\${LD_LIBRARY_PATH} --env PYTHONPATH ${PYTHONPATH}:\${PYTHONPATH} --env PATH ${PATH}:\${PATH} --env CONDA_PREFIX ${CONDA_PREFIX}:\${CONDA_PREFIX} --env CONDA_DEFAULT_ENV ${CONDA_DEFAULT_ENV}:\${CONDA_DEFAULT_ENV} --env YASP_VENV_SH ${YASP_VENV_SH} --env YASP_VENV_TYPE ${YASP_VENV_TYPE}
 
 if [ $? -ne 0 ]; then
 		echo "Failed to install ipykernel"
